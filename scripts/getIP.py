cdfdfcd4e05ed539f538296d067850c89a2eb1bb
SeaWolfos <link157@protonmail.com>
2020-03-17 23:46:37 +0000

    two useful ip scripts

---
+++ scripts/getIP.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from ipdata import ipdata
+from pprint import pprint
+with open('../secrets/ipdata.co.key') as key:
+    key.readline().strip()
+    ipdata = ipdata.IPData(key)
+    ipinput = raw_input("Enter IP address: ")
+    response = ipdata.lookup(ipinput)
